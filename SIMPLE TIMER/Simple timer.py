82e36d120ccf78c7c26f89be28d739017b7a40d0
Abdulhafiz Dada <dadaabdulhafiz0306@gmail.com>
2024-02-18 12:22:28 -0700

    Added sound

--- SIMPLE TIMER/Simple timer.py
+++ SIMPLE TIMER/Simple timer.py
@@ -1,5 +1,7 @@
+from playsound import playsound
 import time
 import os
+
 
 timeNeeded = int(input("Input how many seconds timer you need:\n"))
 
@@ -7,4 +9,9 @@ for i in (range(timeNeeded+1)):
     os.system("cls")
     print(f"{i:0>2}")
     time.sleep(0.98)
-    
+           
+
+
+os.system("cls")    
+print("Time Up!")
+playsound("SIMPLE TIMER\Alarm beep.mp3")
