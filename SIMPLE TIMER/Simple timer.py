a94393c92e48a8ee20103c7c59e5c7df2699850b
Abdulhafiz Dada <dadaabdulhafiz0306@gmail.com>
2024-02-19 20:27:15 -0700

    Added win10toast notifier

--- SIMPLE TIMER/Simple timer.py
+++ SIMPLE TIMER/Simple timer.py
@@ -1,7 +1,7 @@
 from playsound import playsound
 import time
 import os
-
+from win10toast import ToastNotifier 
 
 timeNeeded = int(input("Input how many seconds timer you need:\n"))
 time.sleep(0.5)
@@ -12,8 +12,10 @@ for i in (range(timeNeeded, -1, -1)):
     print(f"{i:0>2}")
     time.sleep(0.99)
 
-os.system("cls")    
-print("Time Up!")
+os.system("cls")
+display = ToastNotifier()
+display.show_toast("Timer!", "The time is up!", threaded=True)  
+
 if vibrate == "Y":
     playsound("SIMPLE TIMER\Vibration.mp3", block = False)
     playsound("SIMPLE TIMER\Alarm beep.mp3")
