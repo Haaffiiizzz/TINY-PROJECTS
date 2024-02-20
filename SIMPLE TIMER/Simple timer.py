5076a9c8911489f2815d6ef5e4621c9d60b5c32f
Abdulhafiz Dada <dadaabdulhafiz0306@gmail.com>
2024-02-19 22:00:00 -0700

    Trying to add tic tac sound while the timer runs

--- SIMPLE TIMER/Simple timer.py
+++ SIMPLE TIMER/Simple timer.py
@@ -2,15 +2,18 @@ from playsound import playsound
 import time
 import os
 from win10toast import ToastNotifier 
+import multiprocessing
 
 timeNeeded = int(input("Input how many seconds timer you need:\n"))
 time.sleep(0.5)
 vibrate = input("Do you want vibrations?(Y/N):").upper()
-
+ticTac = multiprocessing.Process(target=playsound("SIMPLE TIMER\Tic Tac.mp3", block = False))
+ticTac.start()
 for i in (range(timeNeeded, -1, -1)):
     os.system("cls")
     print(f"{i:0>2}")
     time.sleep(0.99)
+ticTac.kill()
 
 os.system("cls")
 display = ToastNotifier()
