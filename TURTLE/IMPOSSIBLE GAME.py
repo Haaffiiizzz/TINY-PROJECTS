aa75f566b29080dc842ec80b1a26bd6b19e6f188
Abdulhafiz Dada <dadaabdulhafiz0306@gmail.com>
2024-03-04 23:17:22 -0700

    fixed somethings and now the ball changes direction on hitting the bars

--- TURTLE/IMPOSSIBLE GAME.py
+++ TURTLE/IMPOSSIBLE GAME.py
@@ -17,15 +17,17 @@ ball.direction = "right"
 
 playerOne = turtle.Turtle()
 playerOne.penup()
-playerOne.goto(-270,0)
+playerOne.goto(270,0)
 playerOne.shape("square")
+playerOne.shapesize(stretch_wid=5, stretch_len=1)
 playerOne.direction = "Stop"
 playerOne.color("blue")
 
 playerTwo = turtle.Turtle()
 playerTwo.penup()
-playerTwo.goto(270,0)
+playerTwo.goto(-270,0)
 playerTwo.shape("square")
+playerTwo.shapesize(stretch_wid=5, stretch_len=1)
 playerTwo.direction = "Stop"
 playerTwo.color("red")
 
@@ -41,27 +43,33 @@ def moveBar(player):
         player.sety(player.ycor() + 20)
     if player.direction == "down":
         player.sety(player.ycor() - 20)
-    
+
+
 def moveBall():
-    
+    if ball.direction == "right":
+        ball.setx(ball.xcor()+30)
+    if ball.direction == "left":
+        ball.setx(ball.xcor()-30)    
 
 
 screen.listen()
-screen.onkeypress(goUp(playerOne), "Up")
-screen.onkeypress(goDown(playerOne), "Down")
-screen.onkeypress(goUp(playerTwo), "w")
-screen.onkeypress(goDown(playerTwo), "z")
+screen.onkeypress(lambda: goUp(playerOne), "Up")
+screen.onkeypress(lambda: goDown(playerOne), "Down")
+screen.onkeypress(lambda: goUp(playerTwo), "w")
+screen.onkeypress(lambda: goDown(playerTwo), "z")
 
 
 
 
 while True:
     screen.update()
-    
-
+    if ball.distance(playerOne) < 10:
+        ball.direction = "left"
+    if ball.distance(playerTwo) < 10:
+        ball.direction = "right"    
+    time.sleep(0.1)
+    moveBall()
     moveBar(playerOne)
     moveBar(playerTwo)
-    time.sleep(0.2)
-
 
 screen.mainloop()       
